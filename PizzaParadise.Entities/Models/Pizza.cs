e2b132577dcbf91ea1b1fb8cf848c377bb0a98e9
Callum Anning <CallumAnning@hotmail.co.uk>
2022-06-08 17:49:55 +0100

    Add project files.

---
+++ PizzaParadise.Entities/Models/Pizza.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PizzaParadise.Entities.Models
+{
+    public class Pizza
+    {
+        public Pizza(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public int Id { get; protected set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        protected class PizzaConfiguration : IEntityTypeConfiguration<Pizza>
+        {
+            public void Configure(EntityTypeBuilder<Pizza> builder)
+            {
+                builder.ToTable("PIZZAS");
+                builder.HasKey(x => x.Id);
+                builder.Property(x => x.Id)
+                    .HasColumnName("BOSS_ID");
+                builder.Property(x => x.Name)
+                    .HasColumnName("NAME")
+                    .HasMaxLength(50);
+                builder.Property(x => x.Description)
+                    .HasColumnName("DESCRIPTION")
+                    .HasMaxLength(255);
+                builder.Property(x => x.Price)
+                    .HasColumnName("PRICE")
+                    .HasPrecision(19, 4);
+            }
+        }
+    }
+}
---
+++ PizzaParadise.Entities/PizzaParadiseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaParadise.Entities.Models;
+
+namespace PizzaParadise.Entities
+{
+    public class PizzaParadiseContext : DbContext
+    {
+        public PizzaParadiseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Pizza> Pizzas => Set<Pizza>();
+    }
+}
