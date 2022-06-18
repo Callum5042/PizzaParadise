b68fbc11c980abafe381d7a07fb7dd97f25006fa
Callum Anning <CallumAnning@hotmail.co.uk>
2022-06-18 13:24:56 +0100

    Login controller

---
+++ PizzaParadise.Mvc/Site/Accounts/AccountsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaParadise.Mvc.Site.Accounts
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            var model = new LoginModel();
+            return View("~/site/accounts/login.cshtml", model);
+        }
+    }
+}
---
+++ PizzaParadise.Mvc/Site/Accounts/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaParadise.Mvc.Site.Accounts
+{
+    public class LoginModel
+    {
+        public string? EmailAddress { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
