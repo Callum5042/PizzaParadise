807aa3ba4903ea7f440224ba62bb6c2ba9abf65b
Callum Anning <CallumAnning@hotmail.co.uk>
2022-06-18 18:57:32 +0100

    Fixed not being able to fetch from the API

--- PizzaParadise.React/ClientApp/src/components/Login.js
+++ PizzaParadise.React/ClientApp/src/components/Login.js
@@ -5,12 +5,50 @@ export class Login extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            emailAddress: "",
+            password: ""
+        };
+
+        this.login = this.login.bind(this);
+        this.handleEmailAddressChange = this.handleEmailAddressChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    async login(e) {
+
+        e.preventDefault();
+        console.log("Login");
+
+        const response = await fetch("accounts");
+        const data = await response.json();
+    }
+
+    handleEmailAddressChange(e) {
+        this.setState({ emailAddress: e.target.value });
+    }
+
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value });
     }
 
     render() {
         return (
             <div>
-                <h1>Login</h1>
+                <form onSubmit={this.login}>
+                    <div className="card">
+                        <h5 className="card-header">
+                            Login
+                        </h5>
+                        <div className="card-body">
+                            <label>Email Address</label>
+                            <input type="text" value={this.state.emailAddress} onChange={this.handleEmailAddressChange} className="form-control" />
+                            <label>Password</label>
+                            <input type="text" value={this.state.password} onChange={this.handlePasswordChange} className="form-control" />
+                            <button type="submit" className="btn btn-primary mt-3">Login</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         );
     }
---
+++ PizzaParadise.React/Controllers/AccountsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaParadise.React.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        public bool Get()
+        {
+            return true;
+        }
+
+        //[HttpPost]
+        //public IActionResult Login()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
